591549cf101ceea81733116b4941894e9a65c8bf
jhgaldino <jhgaldino@pronton.me>
2025-01-09 22:35:03 -0300

    adicionando CORS do Angular

--- XptoAPI/src/Program.cs
+++ XptoAPI/src/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddScoped<IValidator<Pedido>, PedidoValidator>();
 builder.Services.AddScoped<IValidator<MenuItem>, MenuItemValidator>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c=>
+builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
@@ -32,6 +32,16 @@ builder.Services.AddSwaggerGen(c=>
     c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
 
 });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -41,7 +51,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AngularPolicy");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
